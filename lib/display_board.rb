e56aa5a2bfdcdc93b7bce1abdb3b012e9793c7de
andreas fürst <andreas_fuerst@bluewin.ch>
2019-01-13 17:36:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,12 @@
 def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   
-  
+end
+display_board
   
   
   # Define a method display_board that prints a 3x3 Tic Tac Toe Board
